c28d58925f1c372b0fb3c5beaab98397de1b58d3
jsh28 <jsh28612@naver.com>
2024-08-30 14:08:28 +0900

    first commit

---
+++ lib/data/location.dart
@@ -0,0 +1,21 @@
+import 'package:geolocator/geolocator.dart';
+
+class MyLocation {
+  double? latitude2;
+  double? longtitude2;
+
+  Future<void> getMyCurrentLocation() async{
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      latitude2 = position.latitude;
+      longtitude2 = position.longitude;
+      print(latitude2);
+      print(longtitude2);
+      print("position $position");
+    } catch (e) {
+      print('There was a problem with the internet connection.');
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:iot_project_berry/screens/loading.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IoT Berry',
+      home: Loading(),
+    );
+  }
+}
